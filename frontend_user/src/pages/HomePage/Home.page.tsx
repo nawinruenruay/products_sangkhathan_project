99ab5bca4f7735e1ec3610b90c9f3997c2e102b7
Nawin <nawin.ruenruay@gmail.com>
2024-08-10 14:52:49 +0700

    Update Home.page.tsx

--- frontend_user/src/pages/HomePage/Home.page.tsx
+++ frontend_user/src/pages/HomePage/Home.page.tsx
@@ -1,33 +1,16 @@
 import axios from "axios";
 import { useEffect, useState, useRef } from "react";
-import { useNavigate } from "react-router-dom";
 import { Api } from "../../Api";
-import {
-  Image,
-  Text,
-  Skeleton,
-  Paper,
-  Flex,
-  Grid,
-  Card,
-  Group,
-  Button,
-  Badge,
-  Modal,
-} from "@mantine/core";
+import { Image, Skeleton, Paper, Flex } from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
 import Autoplay from "embla-carousel-autoplay";
 
 import classes from "./Home.module.css";
 
 export function HomePage() {
-  const nav = useNavigate();
   const autoplay = useRef(Autoplay({ delay: 3000 }));
   const [Banner, setBanner] = useState([]);
-  const [Products, setProducts] = useState([]);
   const [LoadingData, setLoadingData] = useState(false);
-  const [ShowIMG, setShowIMG] = useState(false);
-  const [ImagePath, setImagePath] = useState("");
 
   const FetchBanner = () => {
     setLoadingData(true);
@@ -40,29 +23,8 @@ export function HomePage() {
     });
   };
 
-  const FetchProducts = () => {
-    setLoadingData(true);
-    axios.get(Api + "/Product/ShowProduct").then((res) => {
-      const data = res.data;
-      if (data.length !== 0) {
-        setProducts(data);
-      }
-      setLoadingData(false);
-    });
-  };
-
-  const ShowImage = (path: string) => {
-    setImagePath(path);
-    setShowIMG(true);
-  };
-
-  const ProductDetail = (v1: string, v2: string) => {
-    nav("/product/" + btoa(v1) + "/" + v2);
-  };
-
   useEffect(() => {
     FetchBanner();
-    FetchProducts();
   }, []);
 
   return (
@@ -95,93 +57,6 @@ export function HomePage() {
           </Paper>
         </>
       )}
-
-      {/* {LoadingData === true ? (
-        <>
-          <Paper radius={8} shadow="sm" mt={20} mb={20}>
-            <Flex direction={"row"} gap={10}>
-              <Skeleton h={200} w={300} />
-              <Skeleton h={200} w={300} />
-              <Skeleton h={200} w={300} />
-              <Skeleton h={200} w={300} />
-            </Flex>
-          </Paper>
-        </>
-      ) : (
-        <>
-          <Grid gutter="md" mt={30} mb={50}>
-            {Products.map((i: any) => (
-              <Grid.Col span={{ base: 6, md: 6, lg: 3 }} key={i.pid}>
-                <Card
-                  shadow="sm"
-                  radius="md"
-                  withBorder
-                  h={"100%"}
-                  className={classes.card}
-                  onClick={() => {
-                    ProductDetail(i.pid, i.pname);
-                  }}
-                >
-                  <Card.Section
-                    style={{ display: "flex", justifyContent: "center" }}
-                  >
-                    <Image
-                      src={Api + i.img}
-                      w={200}
-                      h={150}
-                      className={classes.image}
-                      onClick={() => {
-                        ShowImage(Api + i.img);
-                      }}
-                    />
-                  </Card.Section>
-
-                  <Group justify="space-between" mt="md" mb="xs">
-                    <Text fw={500}>{i.pname}</Text>
-                    <Badge color="red">มีสินค้า {i.qty} ชิ้น</Badge>
-                  </Group>
-
-                  <Text size="sm" c="dimmed">
-                    {i.price.toLocaleString()} บาท
-                  </Text>
-
-                  <Button
-                    fullWidth
-                    mt="md"
-                    radius="md"
-                    onClick={() => {
-                      ProductDetail(i.pid, i.pname);
-                    }}
-                  >
-                    รายละเอียดสินค้า
-                  </Button>
-                </Card>
-              </Grid.Col>
-            ))}
-          </Grid>
-        </>
-      )} */}
-
-      <Modal
-        onClose={() => {
-          setShowIMG(false);
-          setImagePath("");
-        }}
-        opened={ShowIMG}
-        centered
-        overlayProps={{
-          backgroundOpacity: 0.55,
-          blur: 3,
-        }}
-        size={"auto"}
-        withCloseButton={false}
-        classNames={{
-          content: classes.customModal,
-          header: classes.customModal,
-        }}
-      >
-        <Image src={ImagePath} />
-      </Modal>
     </>
   );
 }
