2a39451dc13cf90b75b6f530c9c527e64f1379dd
Nawin <nawin.ruenruay@gmail.com>
2024-08-11 19:17:36 +0700

    Update Contact.page.tsx

--- frontend_user/src/pages/ContactPage/Contact.page.tsx
+++ frontend_user/src/pages/ContactPage/Contact.page.tsx
@@ -1,14 +1,10 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { Image, Text, Paper, Grid, Group } from "@mantine/core";
-import { Carousel } from "@mantine/carousel";
-import Autoplay from "embla-carousel-autoplay";
 import { useDocumentTitle } from "@mantine/hooks";
 import banner from "../../assets/img/banner-contact.png";
-import classes from "./Contact.module.css";
 
 export function ContactPage() {
   useDocumentTitle("ติดต่อสอบถาม | ศูนย์ร่มโพธิ์ร่มไทรวัยดอกลำดวน");
-  const autoplay = useRef(Autoplay({ delay: 3000 }));
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -17,17 +13,7 @@ export function ContactPage() {
   return (
     <>
       <Paper radius={8} shadow="sm">
-        <Carousel
-          withIndicators
-          plugins={[autoplay.current]}
-          onMouseEnter={autoplay.current.stop}
-          onMouseLeave={autoplay.current.reset}
-          classNames={classes}
-        >
-          <Carousel.Slide>
-            <Image src={banner} radius={8} />
-          </Carousel.Slide>
-        </Carousel>
+        <Image src={banner} radius={8} h={350} />
       </Paper>
       <Grid gutter="md" mt={20}>
         <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
