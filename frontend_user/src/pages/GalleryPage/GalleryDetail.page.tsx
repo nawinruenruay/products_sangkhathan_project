c7e66493eaef776a4bfcf3abee324ab324e6f4f6
Nawin <nawin.ruenruay@gmail.com>
2024-08-15 18:10:28 +0700

    Update GalleryDetail.page.tsx

--- frontend_user/src/pages/GalleryPage/GalleryDetail.page.tsx
+++ frontend_user/src/pages/GalleryPage/GalleryDetail.page.tsx
@@ -7,7 +7,6 @@ import {
   Text,
   Skeleton,
   Paper,
-  Flex,
   Grid,
   Group,
   Card,
@@ -29,9 +28,9 @@ export function GalleryDetailPage() {
   const { v1, v2 } = useParams<{ v1: any; v2: any }>();
   const icon = <IconInfoCircle />;
   const [LoadingData, setLoadingData] = useState(false);
-  const [Data, setData] = useState([]);
+  const [Data, setData] = useState<any[]>([]);
 
-  const LoadData = (v1: any) => {
+  const LoadData = (v1: string) => {
     setLoadingData(true);
     axios
       .post(Api + "Activity/postShowactivity/", {
@@ -46,7 +45,7 @@ export function GalleryDetailPage() {
       });
   };
 
-  const formatDateThai = (dateStr: any) => {
+  const formatDateThai = (dateStr: string) => {
     const months = [
       "มกราคม",
       "กุมภาพันธ์",
@@ -99,78 +98,80 @@ export function GalleryDetailPage() {
   }, [Data]);
 
   return (
-    <>
-      <Container size={"1200px"}>
-        {LoadingData === true ? (
-          <>
-            <Paper radius={8} shadow="sm" p={10}>
-              <Flex direction={"column"} gap={10} mb={20}>
-                <Skeleton w={"100%"} h={300} />
-              </Flex>
-              <Flex direction={"row"} gap={10}>
-                <Skeleton h={200} w={300} />
-                <Skeleton h={200} w={300} />
-                <Skeleton h={200} w={300} />
-                <Skeleton h={200} w={300} />
-              </Flex>
-            </Paper>
-          </>
-        ) : (
-          <>
-            <Breadcrumbs
-              separatorMargin={"xs"}
-              ml={20}
-              mb={20}
-              separator={<IconChevronRight stroke={1} />}
-            >
-              {items}
-            </Breadcrumbs>
-            {Data.filter(
-              (item: any, index, self) =>
-                index ===
-                self.findIndex((t: any) => t.act_detail === item.act_detail)
-            ).map((i: any) => (
-              <Blockquote
-                cite={
-                  <>
-                    <Group gap={2}>
-                      <IconCalendarMonth size={20} />
-                      {formatDateThai(i.act_date)}
-                    </Group>
-                  </>
-                }
-                icon={icon}
-                key={i.act_id}
-              >
-                <Text size={"25px"}> {v2} </Text>
-                <Text mt={15}>{i.act_detail}</Text>
-              </Blockquote>
-            ))}
+    <Container size={"1200px"}>
+      {LoadingData ? (
+        <Paper radius={8} shadow="sm" p={10}>
+          <Skeleton height={20} width="50%" mb={20} ml={20} />
 
-            <Grid gutter="md" mt={50} mb={50}>
-              {Data.map((i: any, key) => (
-                <Grid.Col span={{ base: 6, md: 6, lg: 3 }} key={key}>
-                  <Card shadow="sm" withBorder>
-                    <Card.Section
-                      style={{
-                        display: "flex",
-                        justifyContent: "center",
-                      }}
-                    >
-                      <a href={Api + i.gal_pic} data-fancybox="gallery">
-                        <Image
-                          src={Api + i.gal_pic}
-                          className={classes.image}
-                        />
-                      </a>
-                    </Card.Section>
-                  </Card>
-                </Grid.Col>
-              ))}
-            </Grid>
-          </>
-        )}
-      </Container>
-    </>
+          <Skeleton height={40} width="70%" mb={10} ml={20} />
+          <Skeleton height={20} width="60%" mb={30} ml={20} />
+
+          <Grid gutter="md" mt={50} mb={50}>
+            {Array.from({ length: Data.length }).map((_, index) => (
+              <Grid.Col span={{ base: 6, md: 6, lg: 3 }} key={index}>
+                <Card shadow="sm" withBorder>
+                  <Card.Section
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Skeleton height={200} width="100%" />
+                  </Card.Section>
+                </Card>
+              </Grid.Col>
+            ))}
+          </Grid>
+        </Paper>
+      ) : (
+        <>
+          <Breadcrumbs
+            separatorMargin={"xs"}
+            ml={20}
+            mb={20}
+            separator={<IconChevronRight stroke={1} />}
+          >
+            {items}
+          </Breadcrumbs>
+          {Data.filter(
+            (item, index, self) =>
+              index === self.findIndex((t) => t.act_detail === item.act_detail)
+          ).map((i) => (
+            <Blockquote
+              cite={
+                <Group gap={2}>
+                  <IconCalendarMonth size={20} />
+                  {formatDateThai(i.act_date)}
+                </Group>
+              }
+              icon={icon}
+              key={i.act_id}
+            >
+              <Text size={"25px"}> {v2} </Text>
+              <Text mt={15}>{i.act_detail}</Text>
+            </Blockquote>
+          ))}
+
+          <Grid gutter="md" mt={50} mb={50}>
+            {Data.map((i, key) => (
+              <Grid.Col span={{ base: 6, md: 6, lg: 3 }} key={key}>
+                <Card shadow="sm" withBorder>
+                  <Card.Section
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <a href={Api + i.gal_pic} data-fancybox="gallery">
+                      <Image src={Api + i.gal_pic} className={classes.image} />
+                    </a>
+                  </Card.Section>
+                </Card>
+              </Grid.Col>
+            ))}
+          </Grid>
+        </>
+      )}
+    </Container>
   );
 }
